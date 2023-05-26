bcd3a3c70f2dc4cb88c23cdbe079af9a40d60dad
Andrew Hodder <aedonetlira@gmail.com>
2023-05-26 17:12:48 -0600

    Initial commit

---
+++ Cyberstar/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Raylib_cs;
+
+Raylib.InitWindow(1200, 800, "Hello, world");
+Raylib.SetWindowMonitor(1);
+
+var texture = Raylib.LoadTexture("assets/textures/atlas/dev_ships.png");
+
+while (!Raylib.WindowShouldClose())
+{
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.BLACK);
+    
+    UpdateGame();
+    PerformPhysics();
+    RenderGame();
+    DrawUi();
+    
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
+
+
+
+
+void UpdateGame()
+{
+}
+
+void PerformPhysics()
+{
+}
+
+void RenderGame()
+{
+    Raylib.DrawTexture(texture, 0, 0, Color.WHITE);
+}
+
+void DrawUi()
+{
+    Raylib.DrawText("Hello, world!", 12, 12, 20, Color.RED);
+}
